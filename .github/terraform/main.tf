312e9ab11992441a5605cc22096db4fda63d393c
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-04-17 09:14:12 -0700

    ci: removes terraform (#577)
    **Commit Message**
    
    This removes the terraform files that haven't been utilized at all. The
    initial motivation was to make the ci reproducible vs the risk of having
    the admin access in the open repo considering the only configuration we
    have is for AWS Bedrock user.
    
    ---------
    
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- .github/terraform/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "envoy-ai-gateway-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
