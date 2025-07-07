yduwcui/ai-gateway

commit 06d09b8dce7250c54142b17b459f052fa399240b

refactor: use Envoy native router (#793)
**Description**

This commit removes the handwritten header matching code from the
extproc, and instead starts utilizing the hardened envoy native router.

Historically, we had only one giant extproc filter where we did all
logics including model name extraction, routing and then body
transformation & upstream authorization. Since #599, we split into two
external processor filters; one sits at the normal HTTP router and the
other is configured at the per-cluster upstream HTTP filter. In theory,
the one at HTTP router has only one job on request path: extracting
model name from the request body. However, due to the historical reason,
the handwritten router logic component remained, and that comes with not
only a maintenance cost (forcing a complex extproc & control plane
orchestration) but also a potential security vulnerability. In fact,
writing header matching logic can be an easy attack surface, so if it's
possible, we should avoid writing our own header matching (routing
logic) but should rely on the battle-tested hardened envoy native
router.

With this commit, now a regex matching is available as well as there's
no difference between HTTPRoute's matching and AIGatewayRoute's matching
implementation. This also opens up a possibility to support path
matching in our rule.

**Related Issues/PRs (if applicable)**

Ref #612 
Ref #73

---------

Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

internal/controller/ai_gateway_route.go
@@ -24,12 +24,10 @@ import (
 	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	aigv1a1 "github.com/envoyproxy/ai-gateway/api/v1alpha1"
-	"github.com/envoyproxy/ai-gateway/filterapi"
 )
 
 const (
 	managedByLabel             = "app.kubernetes.io/managed-by"
-	selectedRouteHeaderKey     = "x-ai-eg-selected-route"
 	hostRewriteHTTPFilterName  = "ai-eg-host-rewrite"
 	aigatewayUUIDAnnotationKey = "aigateway.envoyproxy.io/uuid"
 	// We use this annotation to ensure that Envoy Gateway reconciles the HTTPRoute when the backend refs change.
@@ -168,10 +166,6 @@ func (c *AIGatewayRouteController) syncAIGatewayRoute(ctx context.Context, aiGat
 	return nil
 }
 
-func routeName(aiGatewayRoute *aigv1a1.AIGatewayRoute, ruleIndex int) filterapi.RouteRuleName {
-	return filterapi.RouteRuleName(fmt.Sprintf("%s-rule-%d", aiGatewayRoute.Name, ruleIndex))
-}
-
 // newHTTPRoute updates the HTTPRoute with the new AIGatewayRoute.
 func (c *AIGatewayRouteController) newHTTPRoute(ctx context.Context, dst *gwapiv1.HTTPRoute, aiGatewayRoute *aigv1a1.AIGatewayRoute) error {
 	rewriteFilters := []gwapiv1.HTTPRouteFilter{{
@@ -182,12 +176,12 @@ func (c *AIGatewayRouteController) newHTTPRoute(ctx context.Context, dst *gwapiv
 			Name:  hostRewriteHTTPFilterName,
 		},
 	}}
-	var rules []gwapiv1.HTTPRouteRule
-	for i, rule := range aiGatewayRoute.Spec.Rules {
-		routeName := routeName(aiGatewayRoute, i)
+	rules := make([]gwapiv1.HTTPRouteRule, 0, len(aiGatewayRoute.Spec.Rules)+1) // +1 for the default rule.
+	for i := range aiGatewayRoute.Spec.Rules {
+		rule := &aiGatewayRoute.Spec.Rules[i]
 		var backendRefs []gwapiv1.HTTPBackendRef
-		for i := range rule.BackendRefs {
-			br := &rule.BackendRefs[i]
+		for j := range rule.BackendRefs {
+			br := &rule.BackendRefs[j]
 			dstName := fmt.Sprintf("%s.%s", br.Name, aiGatewayRoute.Namespace)
 			backend, err := c.backend(ctx, aiGatewayRoute.Namespace, br.Name)
 			if err != nil {
@@ -200,13 +194,15 @@ func (c *AIGatewayRouteController) newHTTPRoute(ctx context.Context, dst *gwapiv
 				}},
 			)
 		}
+		var matches []gwapiv1.HTTPRouteMatch
+		for j := range rule.Matches {
+			matches = append(matches, gwapiv1.HTTPRouteMatch{Headers: rule.Matches[j].Headers})
+		}
 		rules = append(rules, gwapiv1.HTTPRouteRule{
 			BackendRefs: backendRefs,
-			Matches: []gwapiv1.HTTPRouteMatch{
-				{Headers: []gwapiv1.HTTPHeaderMatch{{Name: selectedRouteHeaderKey, Value: string(routeName)}}},
-			},
-			Filters:  rewriteFilters,
-			Timeouts: rule.GetTimeoutsOrDefault(),
+			Matches:     matches,
+			Filters:     rewriteFilters,
+			Timeouts:    rule.GetTimeoutsOrDefault(),
 		})
 	}
 

cmd/aigw/testdata/translate_basic.out.yaml
@@ -92,8 +92,9 @@ spec:
           type: ExtensionRef
       matches:
         - headers:
-            - name: x-ai-eg-selected-route
-              value: envoy-ai-gateway-basic-rule-0
+            - name: x-ai-eg-model
+              type: Exact
+              value: gpt-4o-mini
       timeouts:
         request: 60s
     - backendRefs:
@@ -108,8 +109,9 @@ spec:
           type: ExtensionRef
       matches:
         - headers:
-            - name: x-ai-eg-selected-route
-              value: envoy-ai-gateway-basic-rule-1
+            - name: x-ai-eg-model
+              type: Exact
+              value: llama3-2-1b-instruct-v1
       timeouts:
         request: 60s
     - backendRefs:
@@ -124,8 +126,9 @@ spec:
           type: ExtensionRef
       matches:
         - headers:
-            - name: x-ai-eg-selected-route
-              value: envoy-ai-gateway-basic-rule-2
+            - name: x-ai-eg-model
+              type: Exact
+              value: some-cool-self-hosted-model
       timeouts:
         request: 60s
     - matches:
@@ -181,58 +184,46 @@ metadata:
   namespace: envoy-gateway-system
 stringData:
   filter-config.yaml: |
+    backends:
+    - auth:
+        apiKey:
+          key: apiKey
+      modelNameOverride: ""
+      name: default/envoy-ai-gateway-basic-openai/route/envoy-ai-gateway-basic/rule/0/ref/0
+      schema:
+        name: OpenAI
+        version: v1
+    - auth:
+        aws:
+          credentialFileLiteral: |
+            [default]
+            aws_access_key_id = AWS_ACCESS_KEY_ID
+            aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+          region: us-east-1
+      modelNameOverride: us.meta.llama3-2-1b-instruct-v1:0
+      name: default/envoy-ai-gateway-basic-aws/route/envoy-ai-gateway-basic/rule/1/ref/0
+      schema:
+        name: AWSBedrock
+    - modelNameOverride: ""
+      name: default/envoy-ai-gateway-basic-testupstream/route/envoy-ai-gateway-basic/rule/2/ref/0
+      schema:
+        name: OpenAI
+        version: v1
     metadataNamespace: io.envoy.ai_gateway
     modelNameHeaderKey: x-ai-eg-model
-    rules:
-    - backends:
-      - auth:
-          apiKey:
-            key: apiKey
-        modelNameOverride: ""
-        name: envoy-ai-gateway-basic-openai.default
-        schema:
-          name: OpenAI
-          version: v1
-      headers:
-      - name: x-ai-eg-model
-        value: gpt-4o-mini
-      modelsCreatedAt: "2025-05-23T00:00:00Z"
-      modelsOwnedBy: openai
-      name: envoy-ai-gateway-basic-rule-0
-    - backends:
-      - auth:
-          aws:
-            credentialFileLiteral: |
-              [default]
-              aws_access_key_id = AWS_ACCESS_KEY_ID
-              aws_secret_access_key = AWS_SECRET_ACCESS_KEY
-            region: us-east-1
-        modelNameOverride: us.meta.llama3-2-1b-instruct-v1:0
-        name: envoy-ai-gateway-basic-aws.default
-        schema:
-          name: AWSBedrock
-      headers:
-      - name: x-ai-eg-model
-        value: llama3-2-1b-instruct-v1
-      modelsCreatedAt: "2025-05-23T00:00:00Z"
-      modelsOwnedBy: aws
-      name: envoy-ai-gateway-basic-rule-1
-    - backends:
-      - modelNameOverride: ""
-        name: envoy-ai-gateway-basic-testupstream.default
-        schema:
-          name: OpenAI
-          version: v1
-      headers:
-      - name: x-ai-eg-model
-        value: some-cool-self-hosted-model
-      modelsCreatedAt: "2025-05-23T00:00:00Z"
-      modelsOwnedBy: Envoy AI Gateway
-      name: envoy-ai-gateway-basic-rule-2
+    models:
+    - CreatedAt: "2025-05-23T00:00:00Z"
+      Name: gpt-4o-mini
+      OwnedBy: openai
+    - CreatedAt: "2025-05-23T00:00:00Z"
+      Name: llama3-2-1b-instruct-v1
+      OwnedBy: aws
+    - CreatedAt: "2025-05-23T00:00:00Z"
+      Name: some-cool-self-hosted-model
+      OwnedBy: Envoy AI Gateway
     schema:
       name: OpenAI
       version: v1
-    selectedRouteHeaderKey: x-ai-eg-selected-route
     uuid: envoy-ai-gateway-basic
 ---
 apiVersion: v1

internal/controller/ai_gateway_route_test.go
@@ -132,7 +132,6 @@ func TestAIGatewayRouterController_syncAIGatewayRoute(t *testing.T) {
 		require.Len(t, updatedHTTPRoute.Spec.Rules[0].BackendRefs, 2)
 		require.Equal(t, "some-backend1", string(updatedHTTPRoute.Spec.Rules[0].BackendRefs[0].BackendRef.Name))
 		require.Equal(t, "some-backend2", string(updatedHTTPRoute.Spec.Rules[0].BackendRefs[1].BackendRef.Name))
-		require.Equal(t, "myroute-rule-0", updatedHTTPRoute.Spec.Rules[0].Matches[0].Headers[0].Value)
 		// Defaulting to the empty path, which shouldn't reach in practice.
 		require.Empty(t, updatedHTTPRoute.Spec.Rules[1].BackendRefs)
 		require.Equal(t, "/", *updatedHTTPRoute.Spec.Rules[1].Matches[0].Path.Value)
@@ -178,6 +177,11 @@ func Test_newHTTPRoute(t *testing.T) {
 					Rules: []aigv1a1.AIGatewayRouteRule{
 						{
 							BackendRefs: []aigv1a1.AIGatewayRouteRuleBackendRef{{Name: "apple", Weight: ptr.To[int32](100)}},
+							Matches: []aigv1a1.AIGatewayRouteRuleMatch{
+								{Headers: []gwapiv1.HTTPHeaderMatch{
+									{Name: "x-test", Value: "rule-0"},
+								}},
+							},
 						},
 						{
 							BackendRefs: []aigv1a1.AIGatewayRouteRuleBackendRef{
@@ -185,10 +189,20 @@ func Test_newHTTPRoute(t *testing.T) {
 								{Name: "apple", Weight: ptr.To[int32](100), Priority: ptr.To[uint32](1)},
 								{Name: "pineapple", Weight: ptr.To[int32](100), Priority: ptr.To[uint32](2)},
 							},
+							Matches: []aigv1a1.AIGatewayRouteRuleMatch{
+								{Headers: []gwapiv1.HTTPHeaderMatch{
+									{Name: "x-test", Value: "rule-1"},
+								}},
+							},
 						},
 						{
 							BackendRefs: []aigv1a1.AIGatewayRouteRuleBackendRef{{Name: "foo", Weight: ptr.To[int32](1)}},
 							Timeouts:    &gwapiv1.HTTPRouteTimeouts{Request: &timeout1, BackendRequest: &timeout2},
+							Matches: []aigv1a1.AIGatewayRouteRuleMatch{
+								{Headers: []gwapiv1.HTTPHeaderMatch{
+									{Name: "x-test", Value: "rule-2"},
+								}},
+							},
 						},
 					},
 				},
@@ -237,7 +251,7 @@ func Test_newHTTPRoute(t *testing.T) {
 			expRules := []gwapiv1.HTTPRouteRule{
 				{
 					Matches: []gwapiv1.HTTPRouteMatch{
-						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: selectedRouteHeaderKey, Value: "myroute-rule-0"}}},
+						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: "x-test", Value: "rule-0"}}},
 					},
 					BackendRefs: []gwapiv1.HTTPBackendRef{{BackendRef: gwapiv1.BackendRef{BackendObjectReference: gwapiv1.BackendObjectReference{Name: "some-backend1", Namespace: refNs}, Weight: ptr.To[int32](100)}}},
 					Timeouts:    &gwapiv1.HTTPRouteTimeouts{Request: &defaultTimeout},
@@ -245,7 +259,7 @@ func Test_newHTTPRoute(t *testing.T) {
 				},
 				{
 					Matches: []gwapiv1.HTTPRouteMatch{
-						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: selectedRouteHeaderKey, Value: "myroute-rule-1"}}},
+						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: "x-test", Value: "rule-1"}}},
 					},
 					BackendRefs: []gwapiv1.HTTPBackendRef{
 						{BackendRef: gwapiv1.BackendRef{BackendObjectReference: gwapiv1.BackendObjectReference{Name: "some-backend2", Namespace: refNs}, Weight: ptr.To[int32](100)}},
@@ -257,7 +271,7 @@ func Test_newHTTPRoute(t *testing.T) {
 				},
 				{
 					Matches: []gwapiv1.HTTPRouteMatch{
-						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: selectedRouteHeaderKey, Value: "myroute-rule-2"}}},
+						{Headers: []gwapiv1.HTTPHeaderMatch{{Name: "x-test", Value: "rule-2"}}},
 					},
 					BackendRefs: []gwapiv1.HTTPBackendRef{{BackendRef: gwapiv1.BackendRef{BackendObjectReference: gwapiv1.BackendObjectReference{Name: "some-backend4", Namespace: refNs}, Weight: ptr.To[int32](1)}}},
 					Timeouts:    &gwapiv1.HTTPRouteTimeouts{Request: &timeout1, BackendRequest: &timeout2},
@@ -318,4 +332,12 @@ func Test_buildPriorityAnnotation(t *testing.T) {
 	}
 	annotation := buildPriorityAnnotation(rules)
 	require.Equal(t, "0:orange:0,0:apple:1,0:pineapple:2", annotation)
+}
+
+func TestAIGatewayRouterController_syncGateway_notFound(t *testing.T) { // This is mostly for coverage.
+	fakeClient := requireNewFakeClientWithIndexes(t)
+	kube := fake2.NewClientset()
+	eventCh := internaltesting.NewControllerEventChan[*gwapiv1.Gateway]()
+	s := NewAIGatewayRouteController(fakeClient, kube, logr.Discard(), eventCh.Ch)
+	s.syncGateway(t.Context(), "ns", "non-exist")
 }

commit 2004c864453c7a0594a1393a11b1b5edabfa3361

controller: return 404 instead of 500 for no matching (#837)
**Description**

Before #793, the case where no matching route found was handled in the
extproc and the 404 immediate response was returned from there, but
after that, it naturally results in the "unreachable" default route and
swallowed the indication of no matching and it made it impossible to
reason about the 500 error on that case. In other words, this fixes the
regression in #793 to return the proper 404 response.

---------

Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

internal/controller/ai_gateway_route.go
@@ -27,9 +27,10 @@ import (
 )
 
 const (
-	managedByLabel             = "app.kubernetes.io/managed-by"
-	hostRewriteHTTPFilterName  = "ai-eg-host-rewrite"
-	aigatewayUUIDAnnotationKey = "aigateway.envoyproxy.io/uuid"
+	managedByLabel                      = "app.kubernetes.io/managed-by"
+	hostRewriteHTTPFilterName           = "ai-eg-host-rewrite"
+	routeNotFoundResponseHTTPFilterName = "ai-eg-route-not-found-response"
+	aigatewayUUIDAnnotationKey          = "aigateway.envoyproxy.io/uuid"
 	// We use this annotation to ensure that Envoy Gateway reconciles the HTTPRoute when the backend refs change.
 	// This will result in metadata being added to the underling Envoy route
 	// @see https://gateway.envoyproxy.io/contributions/design/metadata/
@@ -93,19 +94,11 @@ func FilterConfigSecretPerGatewayName(gwName, gwNamespace string) string {
 	return fmt.Sprintf("%s-%s", gwName, gwNamespace)
 }
 
-// syncAIGatewayRoute is the main logic for reconciling the AIGatewayRoute resource.
-// This is decoupled from the Reconcile method to centralize the error handling and status updates.
-func (c *AIGatewayRouteController) syncAIGatewayRoute(ctx context.Context, aiGatewayRoute *aigv1a1.AIGatewayRoute) error {
-	// Check if the HTTPRouteFilter exists in the namespace.
-	var httpRouteFilter egv1a1.HTTPRouteFilter
-	err := c.client.Get(ctx,
-		client.ObjectKey{Name: hostRewriteHTTPFilterName, Namespace: aiGatewayRoute.Namespace}, &httpRouteFilter)
-	if apierrors.IsNotFound(err) {
-		httpRouteFilter = egv1a1.HTTPRouteFilter{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      hostRewriteHTTPFilterName,
-				Namespace: aiGatewayRoute.Namespace,
-			},
+// defaultHTTPRouteFilters returns the default HTTPRouteFilters that are required for the AIGatewayRoute to function.
+func defaultHTTPRouteFilters(ns string) []*egv1a1.HTTPRouteFilter {
+	return []*egv1a1.HTTPRouteFilter{
+		{
+			ObjectMeta: metav1.ObjectMeta{Name: hostRewriteHTTPFilterName, Namespace: ns},
 			Spec: egv1a1.HTTPRouteFilterSpec{
 				URLRewrite: &egv1a1.HTTPURLRewriteFilter{
 					Hostname: &egv1a1.HTTPHostnameModifier{
@@ -113,18 +106,48 @@ func (c *AIGatewayRouteController) syncAIGatewayRoute(ctx context.Context, aiGat
 					},
 				},
 			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{Name: routeNotFoundResponseHTTPFilterName, Namespace: ns},
+			Spec: egv1a1.HTTPRouteFilterSpec{
+				DirectResponse: &egv1a1.HTTPDirectResponseFilter{
+					StatusCode: ptr.To(404),
+					Body: &egv1a1.CustomResponseBody{
+						Inline: ptr.To(
+							// "Likely" since the matching rule can be arbitrary, not necessarily matching on the model name.
+							`No matching route found. It is likely that the model specified your request is not configured in the Gateway.`,
+						),
+					},
+				},
+			},
+		},
+	}
+}
+
+// syncAIGatewayRoute is the main logic for reconciling the AIGatewayRoute resource.
+// This is decoupled from the Reconcile method to centralize the error handling and status updates.
+func (c *AIGatewayRouteController) syncAIGatewayRoute(ctx context.Context, aiGatewayRoute *aigv1a1.AIGatewayRoute) error {
+	// Check if the static default HTTPRouteFilters exist in the namespace.
+	filters := defaultHTTPRouteFilters(aiGatewayRoute.Namespace)
+	for _, base := range filters {
+		var f egv1a1.HTTPRouteFilter
+		if err := c.client.Get(ctx, client.ObjectKey{Name: base.Name, Namespace: base.Namespace}, &f); err != nil {
+			if apierrors.IsNotFound(err) {
+				// Create the filter if it does not exist.
+				if err = c.client.Create(ctx, base); err != nil {
+					return fmt.Errorf("failed to create HTTPRouteFilter %s: %w", f.Name, err)
+				}
+				c.logger.Info("Created HTTPRouteFilter", "name", f.Name, "namespace", f.Namespace)
+			} else {
+				return fmt.Errorf("failed to get HTTPRouteFilter %s: %w", base.Name, err)
+			}
 		}
-		if err = c.client.Create(ctx, &httpRouteFilter); err != nil {
-			return fmt.Errorf("failed to create HTTPRouteFilter: %w", err)
-		}
-	} else if err != nil {
-		return fmt.Errorf("failed to get HTTPRouteFilter: %w", err)
 	}
 
 	// Check if the HTTPRoute exists.
 	c.logger.Info("syncing AIGatewayRoute", "namespace", aiGatewayRoute.Namespace, "name", aiGatewayRoute.Name)
 	var httpRoute gwapiv1.HTTPRoute
-	err = c.client.Get(ctx, client.ObjectKey{Name: aiGatewayRoute.Name, Namespace: aiGatewayRoute.Namespace}, &httpRoute)
+	err := c.client.Get(ctx, client.ObjectKey{Name: aiGatewayRoute.Name, Namespace: aiGatewayRoute.Namespace}, &httpRoute)
 	existingRoute := err == nil
 	if apierrors.IsNotFound(err) {
 		// This means that this AIGatewayRoute is a new one.
@@ -206,20 +229,18 @@ func (c *AIGatewayRouteController) newHTTPRoute(ctx context.Context, dst *gwapiv
 		})
 	}
 
-	// Adds the default route rule with "/" path. This is necessary because Envoy's router selects the backend
-	// before entering the filters. So, all requests would result in a 404 if there is no default route. In practice,
-	// this default route is not used because our AI Gateway filters is the one who actually calculates the route based
-	// on the given Rules. If it doesn't match any backend, 404 will be returned from the AI Gateway filter as an immediate
-	// response.
-	//
-	// In other words, this default route is an implementation detail to make the Envoy router happy and does not affect
-	// the actual routing at all.
-	if len(rules) > 0 {
-		rules = append(rules, gwapiv1.HTTPRouteRule{
-			Name:    ptr.To[gwapiv1.SectionName]("unreachable"),
-			Matches: []gwapiv1.HTTPRouteMatch{{Path: &gwapiv1.HTTPPathMatch{Value: ptr.To("/")}}},
-		})
-	}
+	rules = append(rules, gwapiv1.HTTPRouteRule{
+		Name:    ptr.To[gwapiv1.SectionName]("route-not-found"),
+		Matches: []gwapiv1.HTTPRouteMatch{{Path: &gwapiv1.HTTPPathMatch{Value: ptr.To("/")}}},
+		Filters: []gwapiv1.HTTPRouteFilter{{
+			Type: gwapiv1.HTTPRouteFilterExtensionRef,
+			ExtensionRef: &gwapiv1.LocalObjectReference{
+				Group: "gateway.envoyproxy.io",
+				Kind:  "HTTPRouteFilter",
+				Name:  routeNotFoundResponseHTTPFilterName,
+			},
+		}},
+	})
 
 	dst.Spec.Rules = rules
 

cmd/aigw/testdata/translate_basic.out.yaml
@@ -134,7 +134,13 @@ spec:
     - matches:
         - path:
             value: /
-      name: unreachable
+      name: route-not-found
+      filters:
+        - extensionRef:
+            group: gateway.envoyproxy.io
+            kind: HTTPRouteFilter
+            name: ai-eg-route-not-found-response
+          type: ExtensionRef
 ---
 apiVersion: gateway.envoyproxy.io/v1alpha1
 kind: EnvoyExtensionPolicy
@@ -176,6 +182,19 @@ spec:
   urlRewrite:
     hostname:
       type: Backend
+---
+apiVersion: gateway.envoyproxy.io/v1alpha1
+kind: HTTPRouteFilter
+metadata:
+  name: ai-eg-route-not-found-response
+  namespace: default
+spec:
+  directResponse:
+    body:
+      inline: No matching route found. It is likely that the model specified your
+        request is not configured in the Gateway.
+      type: null
+    statusCode: 404
 ---
 apiVersion: v1
 kind: Secret

internal/controller/ai_gateway_route_test.go
@@ -142,6 +142,12 @@ func TestAIGatewayRouterController_syncAIGatewayRoute(t *testing.T) {
 	err := s.client.Get(t.Context(), client.ObjectKey{Name: hostRewriteHTTPFilterName, Namespace: "ns1"}, &f)
 	require.NoError(t, err)
 	require.Equal(t, hostRewriteHTTPFilterName, f.Name)
+
+	// Also check the default route not found response filter.
+	var notFoundFilter egv1a1.HTTPRouteFilter
+	err = s.client.Get(t.Context(), client.ObjectKey{Name: routeNotFoundResponseHTTPFilterName, Namespace: "ns1"}, &notFoundFilter)
+	require.NoError(t, err)
+	require.Equal(t, routeNotFoundResponseHTTPFilterName, notFoundFilter.Name)
 }
 
 func Test_newHTTPRoute(t *testing.T) {
@@ -279,8 +285,18 @@ func Test_newHTTPRoute(t *testing.T) {
 				},
 				{
 					// The default rule.
-					Name:    ptr.To[gwapiv1.SectionName]("unreachable"),
+					Name:    ptr.To[gwapiv1.SectionName]("route-not-found"),
 					Matches: []gwapiv1.HTTPRouteMatch{{Path: &gwapiv1.HTTPPathMatch{Value: ptr.To("/")}}},
+					Filters: []gwapiv1.HTTPRouteFilter{
+						{
+							Type: gwapiv1.HTTPRouteFilterExtensionRef,
+							ExtensionRef: &gwapiv1.LocalObjectReference{
+								Group: "gateway.envoyproxy.io",
+								Kind:  "HTTPRouteFilter",
+								Name:  routeNotFoundResponseHTTPFilterName,
+							},
+						},
+					},
 				},
 			}
 			require.Equal(t, expRules, httpRoute.Spec.Rules)

tests/e2e/testupstream_test.go
@@ -1,0 +1,116 @@
+// Copyright Envoy AI Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+//go:build test_e2e
+
+package e2e
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/envoyproxy/ai-gateway/tests/internal/testupstreamlib"
+)
+
+// TestWithTestUpstream tests the end-to-end functionality of the AI Gateway with the testupstream server.
+func TestWithTestUpstream(t *testing.T) {
+	const manifest = "testdata/testupstream.yaml"
+	require.NoError(t, kubectlApplyManifest(t.Context(), manifest))
+
+	const egSelector = "gateway.envoyproxy.io/owning-gateway-name=translation-testupstream"
+	requireWaitForGatewayPodReady(t, egSelector)
+
+	t.Run("/chat/completions", func(t *testing.T) {
+		for _, tc := range []struct {
+			name      string
+			modelName string
+			// expHost is the expected host that the request should be forwarded to the testupstream server.
+			// Assertion will be performed in the testupstream server.
+			expHost string
+			// expTestUpstreamID is the expected testupstream ID that the request should be forwarded to.
+			// This is used to differentiate between different testupstream instances.
+			// Assertion will be performed in the testupstream server.
+			expTestUpstreamID string
+			// expPath is the expected path that the request should be forwarded to the testupstream server.
+			// Assertion will be performed in the testupstream server.
+			expPath string
+			// fakeResponseBody is the body that the testupstream server will return when the request is made.
+			fakeResponseBody string
+			// expStatus is the expected HTTP status code for the test case.
+			expStatus int
+			// expResponseBody is the expected response body for the test case. This is optional and can be empty.
+			expResponseBody string
+		}{
+			{
+				name:              "openai",
+				modelName:         "some-cool-model",
+				expTestUpstreamID: "primary",
+				expPath:           "/v1/chat/completions",
+				expHost:           "testupstream.default.svc.cluster.local",
+				fakeResponseBody:  `{"choices":[{"message":{"content":"This is a test."}}]}`,
+				expStatus:         200,
+			},
+			{
+				name:              "aws-bedrock",
+				modelName:         "another-cool-model",
+				expTestUpstreamID: "canary",
+				expHost:           "testupstream-canary.default.svc.cluster.local",
+				expPath:           "/model/another-cool-model/converse",
+				fakeResponseBody:  `{"output":{"message":{"content":[{"text":"response"},{"text":"from"},{"text":"assistant"}],"role":"assistant"}},"stopReason":null,"usage":{"inputTokens":10,"outputTokens":20,"totalTokens":30}}`,
+				expStatus:         200,
+			},
+			{
+				name:            "openai",
+				modelName:       "non-existent-model",
+				expStatus:       404,
+				expResponseBody: `No matching route found. It is likely that the model specified your request is not configured in the Gateway.`,
+			},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				require.Eventually(t, func() bool {
+					fwd := requireNewHTTPPortForwarder(t, egNamespace, egSelector, egDefaultServicePort)
+					defer fwd.kill()
+
+					req, err := http.NewRequest(http.MethodPost, fwd.address()+"/v1/chat/completions", strings.NewReader(fmt.Sprintf(
+						`{"messages":[{"role":"user","content":"Say this is a test"}],"model":"%s"}`,
+						tc.modelName)))
+					require.NoError(t, err)
+					req.Header.Set(testupstreamlib.ResponseBodyHeaderKey, base64.StdEncoding.EncodeToString([]byte(tc.fakeResponseBody)))
+					req.Header.Set(testupstreamlib.ExpectedPathHeaderKey, base64.StdEncoding.EncodeToString([]byte(tc.expPath)))
+					req.Header.Set(testupstreamlib.ExpectedHostKey, tc.expHost)
+					req.Header.Set(testupstreamlib.ExpectedTestUpstreamIDKey, tc.expTestUpstreamID)
+
+					resp, err := http.DefaultClient.Do(req)
+					if err != nil {
+						t.Logf("error: %v", err)
+						return false
+					}
+					defer func() { _ = resp.Body.Close() }()
+					body, err := io.ReadAll(resp.Body)
+					if err != nil {
+						t.Logf("error reading response body: %v", err)
+						return false
+					}
+					if resp.StatusCode != tc.expStatus {
+						t.Logf("unexpected status code: %d (expected %d), body: %s", resp.StatusCode, tc.expStatus, body)
+						return false
+					}
+					if tc.expResponseBody != "" && string(body) != tc.expResponseBody {
+						t.Logf("unexpected response body: %s (expected %s)", body, tc.expResponseBody)
+						return false
+					}
+					return true
+				}, 10*time.Second, 1*time.Second)
+			})
+		}
+	})
+}

tests/e2e/translation_testupstream_test.go
@@ -1,99 +1,0 @@
-// Copyright Envoy AI Gateway Authors
-// SPDX-License-Identifier: Apache-2.0
-// The full text of the Apache license is available in the LICENSE file at
-// the root of the repo.
-
-//go:build test_e2e
-
-package e2e
-
-import (
-	"context"
-	"encoding/base64"
-	"testing"
-	"time"
-
-	"github.com/openai/openai-go"
-	"github.com/openai/openai-go/option"
-	"github.com/stretchr/testify/require"
-
-	"github.com/envoyproxy/ai-gateway/tests/internal/testupstreamlib"
-)
-
-// TestTranslationWithTestUpstream tests the translation with the test upstream.
-func TestTranslationWithTestUpstream(t *testing.T) {
-	const manifest = "testdata/translation_testupstream.yaml"
-	require.NoError(t, kubectlApplyManifest(t.Context(), manifest))
-
-	const egSelector = "gateway.envoyproxy.io/owning-gateway-name=translation-testupstream"
-	requireWaitForGatewayPodReady(t, egSelector)
-
-	t.Run("/chat/completions", func(t *testing.T) {
-		for _, tc := range []struct {
-			name              string
-			modelName         string
-			expHost           string
-			expTestUpstreamID string
-			expPath           string
-			fakeResponseBody  string
-		}{
-			{
-				name:              "openai",
-				modelName:         "some-cool-model",
-				expTestUpstreamID: "primary",
-				expPath:           "/v1/chat/completions",
-				expHost:           "testupstream.default.svc.cluster.local",
-				fakeResponseBody:  `{"choices":[{"message":{"content":"This is a test."}}]}`,
-			},
-			{
-				name:              "aws-bedrock",
-				modelName:         "another-cool-model",
-				expTestUpstreamID: "canary",
-				expHost:           "testupstream-canary.default.svc.cluster.local",
-				expPath:           "/model/another-cool-model/converse",
-				fakeResponseBody:  `{"output":{"message":{"content":[{"text":"response"},{"text":"from"},{"text":"assistant"}],"role":"assistant"}},"stopReason":null,"usage":{"inputTokens":10,"outputTokens":20,"totalTokens":30}}`,
-			},
-		} {
-			t.Run(tc.name, func(t *testing.T) {
-				require.Eventually(t, func() bool {
-					fwd := requireNewHTTPPortForwarder(t, egNamespace, egSelector, egDefaultServicePort)
-					defer fwd.kill()
-
-					ctx, cancel := context.WithTimeout(t.Context(), 10*time.Second)
-					defer cancel()
-
-					t.Logf("modelName: %s", tc.modelName)
-					client := openai.NewClient(option.WithBaseURL(fwd.address()+"/v1/"),
-						option.WithHeader(
-							testupstreamlib.ExpectedTestUpstreamIDKey, tc.expTestUpstreamID),
-						option.WithHeader(
-							testupstreamlib.ExpectedPathHeaderKey, base64.StdEncoding.EncodeToString([]byte(tc.expPath))),
-						option.WithHeader(testupstreamlib.ResponseBodyHeaderKey,
-							base64.StdEncoding.EncodeToString([]byte(tc.fakeResponseBody)),
-						),
-						option.WithHeader(testupstreamlib.ExpectedHostKey, tc.expHost),
-					)
-
-					chatCompletion, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
-						Messages: []openai.ChatCompletionMessageParamUnion{
-							openai.UserMessage("Say this is a test"),
-						},
-						Model: tc.modelName,
-					})
-					if err != nil {
-						t.Logf("error: %v", err)
-						return false
-					}
-					var choiceNonEmpty bool
-					for _, choice := range chatCompletion.Choices {
-						t.Logf("choice: %s", choice.Message.Content)
-						if choice.Message.Content != "" {
-							choiceNonEmpty = true
-						}
-					}
-					return choiceNonEmpty
-				}, 10*time.Second, 1*time.Second)
-			})
-		}
-	})
-}