805162c7aa0e5e79d971b86adb953f854b46b371
Erica Hughberg <erica.sundberg.90@gmail.com>
2025-10-06 18:58:31 -0400

    site: fix dependency versions and config for site (#1286)
    **Description**
    
    Update version dependencies and tidy up configurations for site.
    
    **Related Issues/PRs (if applicable)**
    N/A
    
    **Special notes for reviewers (if applicable)**
    - `onBrokenMarkdownLinks` config moving and had a deprecation warning.
    - `tags` used in published blog posts but missing from `tags.yml`, and
    had a warning
    
    ---------
    
    Signed-off-by: Erica Hughberg <erica.sundberg.90@gmail.com>

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -59,7 +59,6 @@ const config: Config = {
   projectName: 'ai-gateway',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -71,6 +70,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn'
+    },
   },
 
   themes: ['@docusaurus/theme-mermaid'],
